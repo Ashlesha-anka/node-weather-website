eaf724ac7be7e138faa1d75bdaa496b6f20e597c
ashlesha <ashlesha.ashture@anka.co.in>
2022-10-06 16:36:48 +0530

    set up app for heroku

--- src/forecast.js
+++ src/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) =>{
         }else if(body.error){
             callback('unable to find location...', undefined);
         } else {
-            callback(undefined, (body.current.weather_descriptions[0] + ' it is currently ' + body.current.temperature + ' degrees out. it feels like ' + body.current.feelslike + ' degrees out.'));
+            callback(undefined, (body.current.weather_descriptions[0] + ' it is currently ' + body.current.temperature + ' degrees out. it feels like ' + body.current.feelslike + ' degrees out. and humidity is ' + body.current.humidity + 'degrees out.'));
         }
     })
 }
